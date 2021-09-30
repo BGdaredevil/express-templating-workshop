ead97052613c61b005a10d67c4bfe220d3a95fe1
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-09-30 14:12:11 +0300

    Fixed simo stuff

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
 module.exports = {
   development: {
     host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || "3030",
-    table: process.env.DB_TABLE || "Cubicl",
-    dBaseUrl:  `mongodb://${process.env.DB_HOST || "localhost"}:${process.env.DB_PORT || "3030"}/${process.env.DB_TABLE || "Cubicl"}`
+    port: process.env.DB_PORT || "27017",
+    appPort: "3030",
+    table: process.env.DB_TABLE || "Cubicle",
+    dBaseUrl: `mongodb://${process.env.DB_HOST || "localhost"}:${process.env.DB_PORT || "27017"}/${
+      process.env.DB_TABLE || "Cubicle"
+    }`,
   },
   production: {},
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || "development";
 const dBase = require("./config/dBase.js").connectDb;
-const config = require("./config/config.js")['development'];
+const config = require("./config/config.js")["development"];
 
 console.log(config.dBaseUrl);
 console.log(env);
@@ -12,5 +12,8 @@ require("./config/routes")(app);
 
 dBase(config.dBaseUrl).then(() => {
   console.log(`DB is connected at ${config.dBaseUrl}. Awaiting zakazi...`);
-  app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+  app.listen(
+    config.appPort,
+    console.log(`Listening on port ${config.appPort}! Now its up to you...`)
+  );
 });
