498149db78469a1d3703a92aa3fb151bf9cffe80
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-09-30 20:45:44 +0300

    sss

--- controllers/accessoryController.js
+++ controllers/accessoryController.js
@@ -11,14 +11,35 @@ const postAcc = (req, res) => {
 
 const serveCube = (req, res) => {
   console.log(req.params.id);
-  CubeModel.findById(req.params.id)
-    .populate("_Accessories")
-    .lean()
-    .then((cube) => {
-      cube.hasAcc = cube._Accessories.length > 0;
-      console.log(cube);
-      res.render("attachAccessory", { cube: cube });
-    });
+
+  console.log(req.params.id);
+  Promise.allSettled([
+    CubeModel.findById(req.params.id).populate("_Accessories").lean(),
+    AccessoryModel.find().lean(),
+  ]).then(([cube, allAcc]) => {
+    cube = cube.value;
+    allAcc = allAcc.value;
+    console.log(allAcc);
+
+    cube._Accessories = allAcc.reduce((acc, e) => {
+      if (!cube._Accessories.some((x) => x._id === e._id)) {
+        acc.push(e);
+      }
+      return acc;
+    }, []);
+
+    cube.hasAcc = cube._Accessories.length > 0;
+    res.render("attachAccessory", { cube: cube });
+  });
+
+  //   CubeModel.findById(req.params.id)
+  //     .populate("_Accessories")
+  //     .lean()
+  //     .then((cube) => {
+  //       cube.hasAcc = cube._Accessories.length > 0;
+  //       console.log(cube);
+  //       res.render("attachAccessory", { cube: cube });
+  //     });
 };
 
 const addAcc = (req, res) => {
