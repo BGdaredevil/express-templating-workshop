d99986bf5aeefa983061e790b86107dae1729a8d
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-09-30 20:47:15 +0300

    sss

--- config/dBase.js
+++ config/dBase.js
@@ -5,7 +5,7 @@ const AccessoryModel = require("../models/accessory.js");
 
 module.exports = {
   connectDb: (url) => {
-    return mongoose.connect(url);
+    return mongoose.connect(url, { user: "root", pass: "root" });
   },
   models: { CubeModel, AccessoryModel },
 };
