cc3453ef59cd09202aa7f70a6d3a9a51f53d912b
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-10-07 15:58:09 +0300

    working on Adding user support

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,28 @@ const router = require("express").Router();
 const authService = require("../services/authService.js");
 
 const registerUser = (req, res) => {
-  console.log(req.body);
-  authService.register(req.body);
+  //   console.log(req.body);
+  try {
+    authService.createUser(req.body).then((r) => {
+      console.log(r);
+      res.render("/");
+    });
+  } catch (err) {
+    res.render("user/register", { error: true, message: err.message });
+  }
+};
+
+const loginUser = (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (err) {
+    res.render("user/login", { error: true, message: err.message });
+  }
 };
 
 router.get("/register", (req, res) => res.render("user/register"));
 router.post("/register", registerUser);
 router.get("/login", (req, res) => res.render("user/login"));
+router.post("/login", loginUser);
 
 module.exports = router;
--- services/authService.js
+++ services/authService.js
@@ -1,3 +1,49 @@
 const userModel = require("../models/user.js");
+const bcrypt = require("bcrypt");
 
-const createUser = (user) => {};
+const saltRounds = 5;
+
+const createUser = (newUser) => {
+  let username = newUser.username.trim();
+  let password = newUser.password.trim();
+  let repeatPassword = newUser.repeatPassword.trim();
+
+  if (username == "" || password == "" || repeatPassword == "") {
+    throw new Error("All fields are mandatory!");
+  }
+
+  if (password !== repeatPassword) {
+    throw new Error("Passwords do not match");
+  }
+
+  return userModel.find({ username: username }).then((list) => {
+    if (list.length > 0) {
+      throw new Error("This username is taken");
+    }
+
+    return bcrypt
+      .hash(password, saltRounds)
+      .then((hashedPass) => userModel.create({ name: username, password: hashedPass }));
+  });
+};
+
+const login = (user) => {
+  let username = user.username.trim();
+  let password = user.password.trim();
+  return userModel.findOne({ username: username }).then((item) => {
+    if (!item) {
+      throw new Error("Username or password do not match");
+    }
+    return bcrypt.compare(password, item.password).then((res) => {
+      if (res) {
+        //wellcome
+      } else {
+        throw new Error("Username or password do not match");
+      }
+    });
+  });
+};
+
+const authService = { createUser, login };
+
+module.exports = authService;
