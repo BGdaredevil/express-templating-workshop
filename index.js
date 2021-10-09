69bc982d66cd56391e92ab68bc3e9651c4302fff
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-10-09 21:21:46 +0300

    асд

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dBase = require("./config/dBase.js").connectDb;
-const config = require("./config/config.js")[process.env.APP_ENV];
+const config = require("./config/config.js")[process.env.APP_ENV || "development"];
 module.exports = config;
 
 const app = require("express")();
